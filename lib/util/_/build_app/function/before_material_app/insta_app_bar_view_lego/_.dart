d776e1d873e25e1a41a724b6194125d2f27f5686
june lee <melodysdreamj@gmail.com>
2024-07-18 16:16:43 +0900

    Initial commit

---
+++ lib/util/_/build_app/function/before_material_app/insta_app_bar_view_lego/_.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../../main.dart';
+
+// @ReadyBeforeMaterialApp()
+// Future<void> readyForInstaAppBarViewLego(BuildContext context) async {}
---
+++ lib/util/_/build_app/widget/my_app/insta_app_bar_view_lego/_.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../../../../../../main.dart';
+
+// @CoverMyApp()
+// Widget Function() coverInstaAppBarViewLego(Widget Function() materialAppBuilder) {
+//   return () => materialAppBuilder();
+// }
